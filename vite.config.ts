39c91a7fa0e5fdc666117df9d19b76fbe92f3984
Abhishek Mangesh Chavan <128388772+AbhishekChavan1@users.noreply.github.com>
2025-03-23 18:40:54 +0000

    tt

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -18,8 +17,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/abhishekchavan1-abhishekchavan/", // Add your GitHub repository name here
   build: {
-    outDir: "docs", // The output directory to be used by GitHub Pages
+    outDir: "dist", // Default output directory for Vercel
   },
 }));
